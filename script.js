81806330e2f6506201461e4d4afdd6403408edc8
Alexandr Kulikov <alexkulik75@gmail.com>
2023-02-22 00:46:27 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const getRandomNumInRange = (min, max) => {
+    const randomNum = (Math.random() * (max - min) + min).toFixed(0)
+    return randomNum
+}
+
+const getTask = () => {
+    const symbol = (Math.random() > 0.5) ? "+" : "-"
+    const task = `${getRandomNumInRange(0, 100)} ${symbol} ${getRandomNumInRange(0, 100)}`
+    gameState.rightAnswer = eval(task)//eval - определяем правильный ответ
+    return task
+}
+
+// функция изменения состояний
+const toggleGameState = () => {
+    gameState.taskInProcess = !gameState.taskInProcess
+}
+
+const gameElements = document.getElementById("My_game").children
+console.log(gameElements)
+
+const title = gameElements[0]
+const userTask = gameElements[1]
+const userAnswer = gameElements[2]
+const btnGame = gameElements[3]
+
+const gameState = {
+    taskInProcess: false,
+    rightAnswer: null,
+}
+
+btnGame.onclick = () => {
+    if (!gameState.taskInProcess) {
+        title.innerText = "Игра началась!"
+        userAnswer.value = null
+        userTask.innerText = getTask()
+        userAnswer.hidden = false
+        //меняю кнопку и меняю состояние
+        btnGame.innerText = "Проверить!"
+        toggleGameState()
+
+    } else {
+        //сравнить ответ пользователя  с правильным
+        const isRight = gameState.rightAnswer == userAnswer.value
+        userTask.innerText = userTask.innerText + "=" + gameState.rightAnswer
+        title.innerText = (isRight) ? "Вы победили!" : "Вы проиграли!"
+        btnGame.innerText = "Начать заново!"
+        toggleGameState()
+    }
+}
